c7fe7d07f15814ab240906da8051d41f46629a5f
takdev <takfarinas.yazid@gmail.com>
2022-12-24 18:32:07 +0100

    second commit

--- welcom_fullstack/src/main/java/com/formation/fulstatck/Operation.java
+++ welcom_fullstack/src/main/java/com/formation/fulstatck/Operation.java
@@ -3,14 +3,16 @@ package com.formation.fulstatck;
 public class Operation {
 
 	public static void main(String[] args) {
-		double variable = 0;
-		try {
-			if (args.length > 1)
-				variable = Double.valueOf(args[0]) * Double.valueOf(args[1]);
-			System.out.print("resulats  = " + variable);
-		} catch (Exception e) {
-			System.out.println("les valeurs sont mal introduites");
-		}
+		if (args.length > 1)
+			try {
+				Operation.mult(Double.valueOf(args[0]), Double.valueOf(args[1]));
+			} catch (Exception e) {
+				System.out.println("les valeurs sont mal introduites");
+			}
+
 	}
 
+	static double mult(double op1, double op2) {
+		return op1 * op2;
+	}
 }
